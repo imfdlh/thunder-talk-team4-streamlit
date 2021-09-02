f41b4f15aa9300d2a8dae76f4d52d0b6145ecb66
imfdlh <fadilahnurimani@gmail.com>
2021-09-02 12:33:10 +0700

    update files v2

--- streamlit_project/app.py
+++ streamlit_project/app.py
@@ -12,12 +12,12 @@ This app predicts the type of iris flower based on its sepal and petal width and
 st.write("""
 \n
 """)
-st.subheader('Input Data')
+st.sidebar.header('Input Data')
 def user_input_features():
-    sepal_length_cm = st.slider('Sepal length (cm)', 4.0, 10.0, 4.0)
-    sepal_width_cm = st.slider('Sepal width (cm)', 2.0, 5.0, 2.0)
-    petal_length_cm = st.slider('Petal length (cm)', 1.0, 10.0, 1.0)
-    petal_width_cm = st.slider('Petal width (cm)', 0.1, 5.0, 0.1)
+    sepal_length_cm = st.sidebar.slider('Sepal length (cm)', 4.0, 10.0, 5.0)
+    sepal_width_cm = st.sidebar.slider('Sepal width (cm)', 2.0, 5.0, 3.0)
+    petal_length_cm = st.sidebar.slider('Petal length (cm)', 1.0, 10.0, 2.0)
+    petal_width_cm = st.sidebar.slider('Petal width (cm)', 0.1, 5.0, 1.1)
     
     data = {
         'sepal_length':sepal_length_cm,
@@ -31,6 +31,12 @@ def user_input_features():
 
 input_df = user_input_features()
 
+st.sidebar.subheader('Links')
+
+st.sidebar.markdown("""
+[Github Repo](https://github.com/imfdlh/thunder-talk-team4-streamlit)
+""")
+
 st.subheader('User Input Features')
 
 st.write(input_df)
@@ -40,11 +46,13 @@ model = pickle.load(open('classifier.pkl', 'rb'))
 prediction = model.predict(input_df)
 prediction_proba = model.predict_proba(input_df)
 
-st.subheader('Prediction')
 class_name = ['setosa', 'versicolor', 'virginica']
 iris_class = np.array(class_name)
-st.write(iris_class[prediction])
+
 prediction_proba_df = pd.DataFrame(prediction_proba, columns=class_name)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba_df)
+
+st.subheader('Prediction')
+st.write(iris_class[prediction])
--- streamlit_project/setup.sh
+++ streamlit_project/setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [theme]\n\
-primaryColor='#F63366'\n\
+primaryColor='#bd4043'\n\
 backgroundColor='#FFFFFF'\n\
 secondaryBackgroundColor='#F0F2F6'\n\
 textColor='#262730'\n\
